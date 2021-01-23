5ab617d65fc9215c58f9f0c2778def20e15de9e2
01397620 <chenyuanhua@sfmail.sf-express.com>
2021-01-23 09:49:12 +0800

    初始化

---
+++ src/test/java/App.java
@@ -0,0 +1,15 @@
+/**
+ * Description：
+ * <br/>
+ * Copyright (c) ， 2020， 顺丰快运 <br/>
+ * This program is protected by copyright laws. <br/>
+ * Date: 2021/1/23
+ *
+ * @author chenyh chenyuanhua@sfmail.sf-express.com
+ * @version : 1.0
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("App");
+    }
+}
