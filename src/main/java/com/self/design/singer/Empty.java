0913ed0c69b30c4c63f53857d69c568eb7ea29a4
chenyh <1393965078@qq.com>
2021-01-27 11:23:20 +0800

    初始化

---
+++ src/main/java/com/self/design/singer/Empty.java
@@ -0,0 +1,15 @@
+package com.self.design.singer;
+
+/**
+ * Description： <br/>
+ * 一个类只负责一项职责：扩展性、维护性
+ *解决方式：一个类一项功能，在这个类中，方法比较少时，可控制到方法的单一原则
+ * Copyright (c) ， 2020， chainch <br/>
+ * This program is protected by copyright laws. <br/>
+ * Date: 2021/1/20 <br/>
+ *
+ * @author chenyh01 <br/>
+ * @version : 1.0 <br/>
+ */
+public class Empty {
+}
