0d54ae63bf2fe387412649e27365d97ce35348d6
chenyh <1393965078@qq.com>
2021-03-13 10:07:42 +0800

    初始化

---
+++ src/main/java/com/self/design/uml/depndicy/PersonDao.java
@@ -0,0 +1,13 @@
+package com.self.design.uml.depndicy;
+
+/**
+ * Description： <br/>
+ * Copyright (c) ， 2020， chainch <br/>
+ * This program is protected by copyright laws. <br/>
+ * Date: 2021/3/9 <br/>
+ *
+ * @author chenyh01 <br/>
+ * @version : 1.0 <br/>
+ */
+public class PersonDao {
+}
---
+++ src/main/java/com/self/design/uml/depndicy/PersonService.java
@@ -0,0 +1,17 @@
+package com.self.design.uml.depndicy;
+
+/**
+ * Description： <br/>
+ * Copyright (c) ， 2020， chainch <br/>
+ * This program is protected by copyright laws. <br/>
+ * Date: 2021/3/9 <br/>
+ *
+ * @author chenyh01 <br/>
+ * @version : 1.0 <br/>
+ */
+public class PersonService {
+    private PersonDao personDao;
+    private void use(){InUser inUser = new InUser();}
+    private ReturnFF returnFF(){return null;}
+    private void param(ParamF paramF){}
+}
